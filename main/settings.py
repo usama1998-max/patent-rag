2d38d1a4c04f80bb67ff39b6756fae0e69aebbfe
usama1998-pro <154095855+usama1998-pro@users.noreply.github.com>
2025-04-05 03:58:14 +0500

    o3-mini added

--- main/settings.py
+++ main/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 if DEBUG is True:
@@ -227,16 +227,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'assets/'
+STATIC_URL = '/assets/'
 
-
-if DEBUG is True:
+if DEBUG:
     STATICFILES_DIRS = [BASE_DIR / "frontend/dist/assets"]
     STATIC_ROOT = BASE_DIR / "staticfiles"
 else:
     STATDIR = "/var/www/projects/patent-rag/"
     STATICFILES_DIRS = [STATDIR + "frontend/dist/assets"]
     STATIC_ROOT = STATDIR + "staticfiles"
+
 
 
 
